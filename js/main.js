e557274bb83e722a64acb8cb792619bbc048b5d9
MihaZhur <mihailzuravlev349@gmail.com>
2021-10-19 10:26:15 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -20,42 +20,38 @@ import {getRandomPositiveInteger} from './utils/get-random-positive-integer.js';
 // const textRandom = 'какой то текст';
 // watchLengthString(textRandom, 140);
 
-let photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20, 21, 22, 23, 24 ,25]
-let descriptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20, 21, 22, 23, 24 ,25]
-let comments = [
+const photos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20, 21, 22, 23, 24 ,25];
+const descriptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20, 21, 22, 23, 24 ,25];
+const comments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
-let names = ['Артем', 'Андрей', 'Сергей', 'Николай', 'Евгений', ]
+const names = ['Артем', 'Андрей', 'Сергей', 'Николай', 'Евгений'];
 
 Array.prototype.getRandomElement = function () {
   return this[Math.floor(Math.random() * this.length)];
 };
 
-let getPost = function() {
+const getPost = function() {
   return{
     id: getRandomPositiveInteger(1, 25),
-    url:'photos/' + photos.getRandomElement(descriptions) + '.jpg',
+    url: `photos/ + ${photos.getRandomElement(descriptions)} + .jpg`,
     description: descriptions.getRandomElement(),
     likes: getRandomPositiveInteger(15, 200),
     comment : {
       id: getRandomPositiveInteger(15, 200),
-      avatar: 'img/avatar-' + getRandomPositiveInteger(1, 6) + '.svg',
+      avatar: `img/avatar- + ${getRandomPositiveInteger(1, 6)} + .svg`,
       message: comments.getRandomElement(),
       name: names.getRandomElement(),
-      }
-   }
-}
+    },
+  };
+};
 
-const similPost = Array.from({length: 25},  getPost)
-console.log(similPost)
+const similPost = Array.from({length: 25},  getPost);
 
-
-
-
-
+console.log(similPost);
