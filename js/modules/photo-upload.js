09a0da9ae82203cfcd41a81ec645658d8f042357
Mihail-Zhuravlev <mz1218@mail.ru>
2021-11-15 12:02:50 +0300

    Правки валидации

--- js/modules/photo-upload.js
+++ js/modules/photo-upload.js
@@ -1,8 +1,13 @@
 import  {escapeKey, body} from './create-big-pictures.js';
+import {textArea, hashtagsInput} from './validation.js';
 
 const imgCploadCancel = document.querySelector('.img-upload__cancel');
 const uploadFile = document.querySelector('#upload-file');
 const imgUploadOverlay = document.querySelector('.img-upload__overlay ');
+textArea.addEventListener('focus', () => textArea.classList.add('text__description--focus'));
+textArea.addEventListener('blur', () => textArea.classList.remove('text__description--focus'));
+hashtagsInput.addEventListener('focus', () => hashtagsInput.classList.add('text__hashtags--focus'));
+hashtagsInput.addEventListener('blur', () => hashtagsInput.classList.remove('text__hashtags--focus'));
 
 uploadFile.addEventListener('change', () => {
   body.classList.add('modal-open');
@@ -10,7 +15,9 @@ uploadFile.addEventListener('change', () => {
 });
 
 const onCloseImageEscKeydown = (evt) => {
-
+  if(textArea.classList.contains('text__description--focus') || hashtagsInput.classList.contains('text__hashtags--focus') ) {
+    return;
+  }
   if (escapeKey(evt)) {
     evt.preventDefault();
     body.classList.remove('modal-open');
@@ -24,6 +31,7 @@ document.addEventListener('keydown', (evt) => {
 });
 
 imgCploadCancel.addEventListener('click', () => {
+
   body.classList.remove('modal-open');
   imgUploadOverlay.classList.add('hidden');
   uploadFile.value = '';
--- js/modules/validation.js
+++ js/modules/validation.js
@@ -2,22 +2,27 @@
 const imgForm = document.querySelector('.img-upload__form');
 const hashtagsInput = document.querySelector('.text__hashtags');
 const textArea = document.querySelector('.text__description');
-const textAreaMaxLength = 140;
+const TEXT_AREA_MAX_LENGHT = 140;
 
 hashtagsInput.addEventListener('input', () => {
   const value = hashtagsInput.value;
   const hashtagsRegExp = /(^#[A-Za-zА-Яа-яЁё0-9]{1,19})$/;
   const hashArray= value.split(' ');
-  const filteredArr = [...new Set(hashArray)];
+  // const filteredArr = [...new Set(hashArray)];
+  const filteredArr = hashArray.some((item) => hashArray.indexOf(item) !== hashArray.lastIndexOf(item));
 
   for (let hashArrI = 0; hashArrI < hashArray.length; hashArrI++) {
-    if (hashArray.length > filteredArr.length) {
+    if (filteredArr) {
       hashtagsInput.setCustomValidity('Один и тот же хэш-тег не может быть использован дважды');
     } else if (hashArray.length > 5) {
       hashtagsInput.setCustomValidity('Не может быть больше 5 хэштэгов');
     } else if (hashtagsRegExp.test(hashArray[hashArrI]) === false) {
       hashtagsInput.setCustomValidity('Хэштэг начинается с # и должен содержать не больше 20 символов');
-    } else {
+    }
+    else if (value === '') {
+      hashtagsInput.setCustomValidity('');
+    }
+    else {
       hashtagsInput.setCustomValidity('');
     }
   }
@@ -28,7 +33,7 @@ hashtagsInput.addEventListener('input', () => {
 textArea.addEventListener('input', () => {
   const valueLength = textArea.value.length;
 
-  if (valueLength > textAreaMaxLength) {
+  if (valueLength > TEXT_AREA_MAX_LENGHT) {
     textArea.setCustomValidity('Введите не больше 140 символов');
   } else {
     textArea.setCustomValidity('');
@@ -43,4 +48,4 @@ const setImgFormSubmit = () => {
 };
 
 
-export {setImgFormSubmit};
+export {setImgFormSubmit, textArea, hashtagsInput};
